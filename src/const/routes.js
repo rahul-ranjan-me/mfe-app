4115f46737454da176ae4af28a431fd047b24b5f
Rahul Ranjan <rahulranjan@Rahuls-MacBook-Pro.local>
2022-06-16 18:51:17 +0530

    Removed duplicate files

--- src/const/routes.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Home from "../pages/home";
-import About from "../pages/about";
-
-export default [
-  { path: "payments", element: <Home /> },
-  { path: "payments/about", element: <About /> },
-];
